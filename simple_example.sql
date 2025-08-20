55d81fd56f095c1dc0f946af6f147e2fc34d4c29
Anna Markova <anna.bloomdesignlab@gmail.com>
2025-08-20 15:19:44 +0200

    Update simple_example.sql

--- simple_example.sql
+++ simple_example.sql
@@ -1,12 +1,12 @@
 CREATE TABLE employees (
     id INT PRIMARY KEY,
-    name VARCHAR(50),
+    name VARCHAR(80),
     job_title VARCHAR(100)
 );
 
-INSERT INTO employees VALUES (1, 'Anna', 'Designer');
+INSERT INTO employees VALUES (1, 'Anna', 'Analyst');
 INSERT INTO employees VALUES (2, 'Mark', 'Developer');
 
 SELECT name, job_title 
 FROM employees
-WHERE job_title = 'Designer';
+WHERE job_title = 'Analyst';
