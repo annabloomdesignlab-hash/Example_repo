fc6a11f824bc33841db94d41f6e878734b55e776
annabloomdesignlab-hash <annabloomdesignlab-hash>
2025-08-20 09:17:00 +0000

    Updated

--- simple_example.sql
+++ simple_example.sql
@@ -1,12 +1,12 @@
 CREATE TABLE employees (
     id INT PRIMARY KEY,
     name VARCHAR(50),
-    role VARCHAR(50)
+    job_title VARCHAR(100)
 );
 
 INSERT INTO employees VALUES (1, 'Anna', 'Designer');
 INSERT INTO employees VALUES (2, 'Mark', 'Developer');
 
-SELECT name, role 
+SELECT name, job_title 
 FROM employees
-WHERE role = 'Designer';
+WHERE job_title = 'Designer';
