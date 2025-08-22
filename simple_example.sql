4e930bf9aa702c2db2464949c7253a2f7af47825
annabloomdesignlab-hash <annabloomdesignlab-hash>
2025-08-22 15:01:37 +0000

    replaced the code

--- simple_example.sql
+++ simple_example.sql
@@ -1,12 +1,1 @@
-CREATE TABLE employees (
-    id INT PRIMARY KEY,
-    name VARCHAR(50),
-    job_title VARCHAR(50)
-);
-
-INSERT INTO employees VALUES (1, 'Anna', 'Designer');
-INSERT INTO employees VALUES (2, 'Mark', 'Developer');
-
-SELECT name, job_title 
-FROM employees
-WHERE job_title = 'Designer';
+print("Hello from main branch")
